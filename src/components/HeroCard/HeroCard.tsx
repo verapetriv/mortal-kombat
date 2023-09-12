e593391cf6f48db412e37eb86de7d8d5b156d6f5
Vira Petriv <vera.petriv98@gmail.com>
2023-09-12 13:06:59 +0300

    feat: create hero card

---
+++ src/components/HeroCard/HeroCard.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { IHero } from '../../data/heroes';
+import cn from 'classnames';
+import styles from './HeroCard.module.scss';
+
+interface IProps {
+  hero: IHero;
+  isActive: boolean;
+  isMainHero: boolean;
+  isChosenMainHero: boolean;
+  isChosenEnemy: boolean;
+  handleClick: () => void;
+}
+
+export const HeroCard: FC<IProps> = ({
+  hero,
+  isActive,
+  isMainHero,
+  isChosenMainHero,
+  isChosenEnemy,
+  handleClick,
+}) => {
+  return (
+    <li
+      className={cn(styles.card, {
+        [styles.activeHero]: isActive && isMainHero,
+        [styles.activeEnemy]: isActive && !isMainHero,
+      })}
+      onClick={handleClick}
+    >
+      <img className={styles.img} src={hero.image} alt={hero.name} />
+      {isChosenMainHero && (
+        <span className={cn(styles.playerNumber, styles.firstNumber)}>1</span>
+      )}
+      {isChosenEnemy && (
+        <span className={cn(styles.playerNumber, styles.secondNumber)}>2</span>
+      )}
+    </li>
+  );
+};
