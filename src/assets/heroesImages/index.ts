0f1ecd30cc8c57a7f33ffb078b8a5e0786bdd3ae
Vira Petriv <vera.petriv98@gmail.com>
2023-09-12 13:02:05 +0300

    feat: add heroes images and background

---
+++ src/assets/heroesImages/index.ts
@@ -0,0 +1,27 @@
+import baraka from './baraka.jpg';
+import cyrax from './cyrax.jpg';
+import jade from './jade.jpg';
+import jax from './jax.png';
+import kano from './kano.jpg';
+import liuKang from './liukang.webp';
+import sector from './sector.jpg';
+import shang from './shang.jpg';
+import sindel from './sindel.jpg';
+import smoke from './smoke.jpeg';
+import sonyaBlade from './sonyablade.jpg';
+import subZero from './subzero.jpg';
+
+export const heroesImages = {
+  baraka,
+  cyrax,
+  jade,
+  jax,
+  kano,
+  liuKang,
+  sector,
+  shang,
+  sindel,
+  smoke,
+  sonyaBlade,
+  subZero,
+};
---
+++ src/assets/heroesPreviews/index.ts
@@ -0,0 +1,27 @@
+import baraka from './baraka-preview.gif';
+import cyrax from './cyrax-preview.gif';
+import jade from './jade-preview.gif';
+import jax from './jax-preview.gif';
+import kano from './kano-preview.gif';
+import liuKang from './liukang-preview.gif';
+import sector from './sector-preview.gif';
+import shang from './shang-preview.webp';
+import sindel from './sindel-preview.gif';
+import smoke from './smoke-preview.gif';
+import sonyaBlade from './sonyablade-preview.gif';
+import subZero from './subzero-preview.gif';
+
+export const heroesPreviews = {
+  baraka,
+  cyrax,
+  jade,
+  jax,
+  kano,
+  liuKang,
+  sector,
+  shang,
+  sindel,
+  smoke,
+  sonyaBlade,
+  subZero,
+};
