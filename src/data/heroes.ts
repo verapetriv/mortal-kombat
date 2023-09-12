ced8e1c7cfaebae5b9f5fa505f4d9a71f114e9d2
Vira Petriv <vera.petriv98@gmail.com>
2023-09-12 21:22:00 +0300

    refactor: remove unnecessary images

--- src/data/heroes.ts
+++ src/data/heroes.ts
@@ -1,13 +1,11 @@
 import { heroesImages } from '../assets/heroesImages';
 import { heroesPreviews } from '../assets/heroesPreviews';
-import { heroesBattles } from '../assets/heroesBattles';
 
 export interface IHero {
   id: number;
   name: string;
   image: string;
   preview: string;
-  battle: string;
 }
 
 export const heroes: IHero[] = [
@@ -16,104 +14,89 @@ export const heroes: IHero[] = [
     name: 'Baraka',
     image: heroesImages.baraka,
     preview: heroesPreviews.baraka,
-    battle: heroesBattles.baraka,
   },
   {
     id: 2,
     name: 'Cyrax',
     image: heroesImages.cyrax,
     preview: heroesPreviews.cyrax,
-    battle: heroesBattles.cyrax,
   },
   {
     id: 3,
     name: 'Jade',
     image: heroesImages.jade,
     preview: heroesPreviews.jade,
-    battle: heroesBattles.jade,
   },
   {
     id: 4,
     name: 'Jax',
     image: heroesImages.jax,
     preview: heroesPreviews.jax,
-    battle: heroesBattles.jax,
   },
   {
     id: 5,
     name: 'Kano',
     image: heroesImages.kano,
     preview: heroesPreviews.kano,
-    battle: heroesBattles.kano,
   },
   {
     id: 6,
     name: 'Liu Kang',
     image: heroesImages.liuKang,
     preview: heroesPreviews.liuKang,
-    battle: heroesBattles.liuKang,
   },
   {
     id: 7,
     name: 'Sektor',
     image: heroesImages.sector,
     preview: heroesPreviews.sector,
-    battle: heroesBattles.sector,
   },
   {
     id: 8,
     name: 'Shang Tsung',
     image: heroesImages.shang,
     preview: heroesPreviews.shang,
-    battle: heroesBattles.shang,
   },
   {
     id: 9,
     name: 'Sindel',
     image: heroesImages.sindel,
     preview: heroesPreviews.sindel,
-    battle: heroesBattles.sindel,
   },
   {
     id: 10,
     name: 'Smoke',
     image: heroesImages.smoke,
     preview: heroesPreviews.smoke,
-    battle: heroesBattles.smoke,
   },
   {
     id: 11,
     name: 'Sonya Blade',
     image: heroesImages.sonyaBlade,
     preview: heroesPreviews.sonyaBlade,
-    battle: heroesBattles.sonyaBlade,
   },
   {
     id: 12,
     name: 'Sub Zero',
     image: heroesImages.subZero,
     preview: heroesPreviews.subZero,
-    battle: heroesBattles.subZero,
   },
   {
     id: 13,
     name: 'Kabal',
     image: heroesImages.kabal,
     preview: heroesPreviews.kabal,
-    battle: heroesBattles.kabal,
   },
   {
     id: 14,
     name: 'Kung Lao',
     image: heroesImages.kungLao,
     preview: heroesPreviews.kungLao,
-    battle: heroesBattles.kungLao,
   },
   {
     id: 15,
     name: 'Sheeva',
     image: heroesImages.sheeva,
     preview: heroesPreviews.sheeva,
-    battle: heroesBattles.sheeva,
   },
 ];
